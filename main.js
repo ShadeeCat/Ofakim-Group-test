ca996df63a203a432aa9562062abdf09b191c5d5
Alex <assulema@gmail.com>
2017-06-29 05:05:34 +0300

    added progress bar (and js), responsiveness, background effects

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function updateCounter() {
+	var start = new Date('2017-06-01');
 	var now = new Date();
-	var end = new Date('2017-09-30');
+	var end = new Date('2017-09-01');
 	var interval = (end - now) / 1000; // seconds until end
 
 	var days = Math.floor(interval / 24 / 60 / 60);
@@ -8,10 +9,15 @@ function updateCounter() {
 	var minutes = Math.floor(interval / 60 % 60);
 	var seconds = Math.floor(interval % 60);
 
+	var percentage = Math.floor((now - start) / (end - start) * 100) + '%';
+
 	$('#days').text(days);
 	$('#hours').text(hours);
 	$('#minutes').text(minutes);
 	$('#seconds').text(seconds);
+
+	$('.progress-label .progress-strip').css('width', percentage);
+	$('.progress-label .progress-percentage').text(percentage);
 }
 
 updateCounter();
